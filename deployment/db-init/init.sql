64e6e728634f7955ad32010a8f021ba0cc5ce356
hailey cho <75298117+hallov012@users.noreply.github.com>
2023-03-31 15:54:38 +0900

    Delete deployment directory

--- deployment/db-init/init.sql
+++
@@ -1,4 +0,0 @@
-create database IF NOT EXISTS `ollenge` collate utf8mb4_general_ci;
-create user 'ollenge'@'%' identified by 'ollenge1010';
-grant all privileges on *.* to ollenge@'%';
-flush privileges;
